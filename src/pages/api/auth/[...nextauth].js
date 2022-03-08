837fd27baf02df9ef2cbb1df5e8ff808f435d324
adham618 <adhamtarek291@gmail.com>
2022-03-08 13:39:18 +0200

    feat: add firebase adpater

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,61 @@
+import { FirebaseAdapter } from '@next-auth/firebase-adapter';
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+  limit,
+  query,
+  runTransaction,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
+import NextAuth from 'next-auth';
+import EmailProvider from 'next-auth/providers/email';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_Storage_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+
+export default NextAuth({
+  adapter: FirebaseAdapter({
+    db,
+    collection,
+    query,
+    getDocs,
+    where,
+    limit,
+    doc,
+    getDoc,
+    addDoc,
+    updateDoc,
+    deleteDoc,
+    runTransaction,
+  }),
+  // Configure one or more authentication providers
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+    // ...add more providers here
+  ],
+});
