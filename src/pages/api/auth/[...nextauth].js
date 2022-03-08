62f84496f39532f59dcb23273aa9891efdf22137
adham618 <adhamtarek291@gmail.com>
2022-03-08 15:05:05 +0200

    feat: add monogodb adapter

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,48 +1,52 @@
-import { FirebaseAdapter } from '@next-auth/firebase-adapter';
-import { getApp, getApps, initializeApp } from 'firebase/app';
-import {
-  addDoc,
-  collection,
-  deleteDoc,
-  doc,
-  getDoc,
-  getDocs,
-  getFirestore,
-  limit,
-  query,
-  runTransaction,
-  updateDoc,
-  where,
-} from 'firebase/firestore';
+//import { FirebaseAdapter } from '@next-auth/firebase-adapter';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+// import { getApp, getApps, initializeApp } from 'firebase/app';
+// import {
+//   addDoc,
+//   collection,
+//   deleteDoc,
+//   doc,
+//   getDoc,
+//   getDocs,
+//   getFirestore,
+//   limit,
+//   query,
+//   runTransaction,
+//   updateDoc,
+//   where,
+// } from 'firebase/firestore';
 import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_Storage_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const db = getFirestore(app);
+import clientPromise from '../../../lib/mongodb';
+
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_Storage_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+// };
+// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// const db = getFirestore(app);
 
 export default NextAuth({
-  adapter: FirebaseAdapter({
-    db,
-    collection,
-    query,
-    getDocs,
-    where,
-    limit,
-    doc,
-    getDoc,
-    addDoc,
-    updateDoc,
-    deleteDoc,
-    runTransaction,
-  }),
+  // adapter: FirebaseAdapter({
+  //   db,
+  //   collection,
+  //   query,
+  //   getDocs,
+  //   where,
+  //   limit,
+  //   doc,
+  //   getDoc,
+  //   addDoc,
+  //   updateDoc,
+  //   deleteDoc,
+  //   runTransaction,
+  // }),
+  adapter: MongoDBAdapter(clientPromise),
   // Configure one or more authentication providers
   providers: [
     EmailProvider({
